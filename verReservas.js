e321caaa0a2cc673850cd0b2253ca920ae49a521
oscartobar857OTB <oscartobar857@gmail.com>
2025-08-12 11:47:47 -0500

    Add files via upload

---
+++ verReservas.js
@@ -0,0 +1,27 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-firestore.js";
+
+// Configuración Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDo3cgRT87JQkV9kgJG2mrPnUkb5HugH9Q",
+  authDomain: "rifasammuel.firebaseapp.com",
+  projectId: "rifasammuel",
+  storageBucket: "rifasammuel.firebasestorage.app",
+  messagingSenderId: "707645550462",
+  appId: "1:707645550462:web:42735685340fb79e5d8d2f",
+  measurementId: "G-W3HY4MHHV4"
+};
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Función para obtener reservas
+async function obtenerReservas() {
+  const querySnapshot = await getDocs(collection(db, "reservas"));
+  querySnapshot.forEach((doc) => {
+    console.log(doc.id, " => ", doc.data());
+  });
+}
+
+obtenerReservas();
